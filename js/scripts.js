e13e32bb1e07043b585af1a97abec9f86253ccc9
Franz R. Sattler <sattler@thphys.uni-heidelberg.de>
2025-04-02 15:16:56 +0200

    Initial commit
    Signed-off-by: Franz R. Sattler <sattler@thphys.uni-heidelberg.de>

---
+++ js/scripts.js
@@ -0,0 +1,58 @@
+/*!
+* Start Bootstrap - Resume v7.0.6 (https://startbootstrap.com/theme/resume)
+* Copyright 2013-2023 Start Bootstrap
+* Licensed under MIT (https://github.com/StartBootstrap/startbootstrap-resume/blob/master/LICENSE)
+*/
+//
+// Scripts
+// 
+
+window.addEventListener('DOMContentLoaded', event => {
+
+    // Activate Bootstrap scrollspy on the main nav element
+    const sideNav = document.body.querySelector('#sideNav');
+    if (sideNav) {
+        new bootstrap.ScrollSpy(document.body, {
+            target: '#sideNav',
+            rootMargin: '0px 0px -40%',
+        });
+    };
+
+    // Collapse responsive navbar when toggler is visible
+    const navbarToggler = document.body.querySelector('.navbar-toggler');
+    const responsiveNavItems = [].slice.call(
+        document.querySelectorAll('#navbarResponsive .nav-link')
+    );
+    responsiveNavItems.map(function (responsiveNavItem) {
+        responsiveNavItem.addEventListener('click', () => {
+            if (window.getComputedStyle(navbarToggler).display !== 'none') {
+                navbarToggler.click();
+            }
+        });
+    });
+
+});
+
+
+
+// Function to load external HTML content into a div
+function loadSection(sectionId, fileName) {
+    fetch(fileName)
+        .then(response => response.text())
+        .then(data => {
+            document.getElementById(sectionId).innerHTML = data;
+
+            // Trigger MathJax reprocessing
+            if (window.MathJax) {
+                MathJax.typesetPromise();
+            }
+        })
+        .catch(error => console.error('Error loading section:', error));
+}
+
+
+// Load the sections into their respective divs
+loadSection('members', 'members.html');
+loadSection('projects', 'projects.html');
+loadSection('news', 'news.html');
+loadSection('publications', 'publications.html');
